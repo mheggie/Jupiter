4ab8bbe5984aa809a2c66c03c75ab5b3758538bb
Mike <mhegs42@yahoo.com.au>
2022-04-20 07:23:47 +1000

    Adds UserModel
    Adds the UserModel auditable entity

---
+++ Domain/Entities/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class UserModel : AuditableEntity
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public int AuthorityId { get; set; }
+    }
+}
