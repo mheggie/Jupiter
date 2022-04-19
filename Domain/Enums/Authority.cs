8e2d976b0e2a5270b40a7f297da085e85670cf21
Mike <mhegs42@yahoo.com.au>
2022-04-20 07:24:19 +1000

    Adds Enums
    Adds the Authority, Role & Status enums

---
+++ Domain/Enums/Authority.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum Authority
+    {
+        Admin=0,
+        General=1,
+        Super=2
+    }
+}
