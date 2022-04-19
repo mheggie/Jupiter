47d5a47a2b42e7d2248f4a04ed941eda8aeeade3
Mike <mhegs42@yahoo.com.au>
2022-04-20 07:22:34 +1000

    Adds IDataAccess
    Adds the IDataAccess interface and the SqliteDataAccess implementation

---
+++ Application/Common/Interfaces/IDataAccess.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Application.Common.Interfaces
+{
+    public interface IDataAccess
+    {
+        List<T> LoadData<T>(string sql, string connectionStringName);
+        void SaveData(string sql, string connectionStringName);
+    }
+}
---
+++ Infrastructure/Persistence/SqliteDataAccess.cs
@@ -0,0 +1,46 @@
+﻿using Application.Common.Interfaces;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace Infrastructure.Persistence
+{
+    public class SqliteDataAccess : IDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqliteDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private string GetConnectionString(string name)
+        {
+            return _config.GetConnectionString(name);
+        }
+
+        public List<T> LoadData<T>(string sql, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection cnn = new SQLiteConnection(connectionString))
+            {
+                List<T> rows = cnn.Query<T>(sql).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData(string sql, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Execute(sql);
+            }
+        }
+    }
+}
